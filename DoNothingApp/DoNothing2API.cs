11022c5be51985719598bc3ec680dc6049ca1103
Victor Chua <victor.chua@nintex.com>
2020-12-22 11:04:04 +0800

    Use OpenTelemetry SDK

--- DoNothingApp/DoNothing2API.cs
+++ DoNothingApp/DoNothing2API.cs
@@ -1,38 +1,29 @@
 using System;
-using System.IO;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using NewRelic.Azure.BindingExtension;
-using Microsoft.Extensions.Primitives;
 
 namespace FunctionApp1
 {
-    public static class DoNothing2API
+    public class DoNothing2API
     {
-        [FunctionName("DoNothing2API")]
-        public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log,
-            ExecutionContext context,
-            [NewRelic.Azure.BindingExtension.NewRelicTelemetry(NewRelicAppName = "DoNothing2")] ICollector<NewRelic.Azure.BindingExtension.NewRelicCollector> collector)
+        private readonly ILogger<DoNothing2API> _logger;
+
+        public DoNothing2API(ILogger<DoNothing2API> logger)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            _logger = logger;
+        }
+
+        [FunctionName("DoNothing2API")]
+        public IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ExecutionContext context)
+        {
+            this._logger.LogInformation("C# HTTP trigger function processed a request.");
 
             string correlationId = string.IsNullOrWhiteSpace(req.Query["id"]) ? Guid.NewGuid().ToString() : req.Query["id"].ToString();
-
-            var newRelicCollector = new NewRelic.Azure.BindingExtension.NewRelicCollector(context);
-            newRelicCollector.GenerateMetrics();
-            newRelicCollector.GenerateTraces(req, System.Diagnostics.ActivityKind.Producer, correlationId);
-            newRelicCollector.TraceCollector.AddTraceTag("tenantId", "microplumbers");
-            newRelicCollector.TraceCollector.AddTraceTag("workflowId", "abcdefg");
-            newRelicCollector.TraceCollector.AddTraceTag("Do", "Nothing");
-            newRelicCollector.TraceCollector.AddTraceTag("correlationId", correlationId);
-
-            collector.Add(newRelicCollector);
 
             string name = req.Query["name"];
 
--- DoNothingApp/DoNothingAPI.cs
+++ DoNothingApp/DoNothingAPI.cs
@@ -1,39 +1,31 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using NewRelic.Azure.BindingExtension;
 using System.Net.Http;
-using Microsoft.Extensions.Primitives;
 
 namespace FunctionApp1
 {
-    public static class DoNothingAPI
+    public class DoNothingAPI
     {
-        [FunctionName("DoNothingAPI")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log,
-            ExecutionContext context,
-            [NewRelic.Azure.BindingExtension.NewRelicTelemetry(NewRelicAppName = "DoNothing1")] ICollector<NewRelic.Azure.BindingExtension.NewRelicCollector> collector)
+        private readonly ILogger<DoNothingAPI> _logger;
+
+        public DoNothingAPI(ILogger<DoNothingAPI> logger)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            _logger = logger;
+        }
+
+        [FunctionName("DoNothingAPI")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ExecutionContext context)
+        {
+            this._logger.LogInformation("C# HTTP trigger function processed a request.");
 
             string correlationId = string.IsNullOrWhiteSpace(req.Query["id"]) ? Guid.NewGuid().ToString() : req.Query["id"].ToString();
-
-            var newRelicCollector = new NewRelic.Azure.BindingExtension.NewRelicCollector(context);
-            newRelicCollector.GenerateMetrics();
-            newRelicCollector.GenerateTraces(req, System.Diagnostics.ActivityKind.Consumer, correlationId);
-            newRelicCollector.TraceCollector.AddTraceTag("tenantId", "microplumbers");
-            newRelicCollector.TraceCollector.AddTraceTag("workflowId", "abcdefg");
-            newRelicCollector.TraceCollector.AddTraceTag("Do", "Nothing");
-            newRelicCollector.TraceCollector.AddTraceTag("correlationId", correlationId);
-
-            collector.Add(newRelicCollector);
 
             string name = req.Query["name"];
 
@@ -41,7 +33,6 @@ namespace FunctionApp1
             {
                 name = await req.ReadAsStringAsync();
             }
-
 
             HttpClient client = new HttpClient();
             HttpResponseMessage response = await client.GetAsync($"http://{Environment.GetEnvironmentVariable("WEBSITE_HOSTNAME")}/api/DoNothing2API?name={name}&id={correlationId}");
